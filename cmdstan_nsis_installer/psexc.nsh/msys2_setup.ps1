60609117e3ecc464b02c033b30efab8c7485e638
Brian Parbhu <brian.parbhu@gmail.com>
2021-04-21 16:46:11 -0400

    moved file to new subfolder

---
+++ cmdstan_nsis_installer/psexc.nsh/msys2_setup.ps1
@@ -0,0 +1,5 @@
+# This is the script that updates MSYS2 for the user
+bash -1 -c  "pacman -Syuu --noconfirm"
+bash -1 -c "pacman -Sy mingw-w64-x86_64-make --noconfirm"
+bash -1 -c "pacman -Syu gcc --noconfirm"
+!include psexec.nsh
