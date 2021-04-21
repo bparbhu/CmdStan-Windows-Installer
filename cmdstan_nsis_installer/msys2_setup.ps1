f39c847192bb528d760d4586dcfdc5b64d22555d
Brian Parbhu <brian.parbhu@gmail.com>
2021-04-21 16:48:22 -0400

    removed file as it now lives in new subfolder

--- cmdstan_nsis_installer/msys2_setup.ps1
+++
@@ -1,5 +0,0 @@
-# This is the script that updates MSYS2 for the user
-bash -l -c  "pacman -Syuu --noconfirm"
-bash -1 -c "pacman -Sy mingw-w64-x86_64-make --noconfirm"
-bash -1 -c "pacman -Syu gcc --noconfirm"
-!include psexec.nsh
